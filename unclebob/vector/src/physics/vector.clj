982f7c90d192a074ed97e36bf7171756c10c9085
Curtis Badke <curtis@badkid.ca>
2012-06-04 05:05:40 -0600

    Fancier subtract impl.

--- unclebob/vector/src/physics/vector.clj
+++ unclebob/vector/src/physics/vector.clj
@@ -7,6 +7,11 @@
   ([] (struct vector 0 0))
   ([x y] (struct vector x y)))
 
+(defn scale
+  [{x :x y :y} factor]
+  (make (* factor x) 
+        (* factor y)))
+
 (defn add
   [v1 v2]
   (make (+ (:x v1) (:x v2)) 
@@ -14,19 +19,13 @@
 
 (defn subtract
   [v1 v2]
-  (make (- (:x v1) (:x v2)) 
-        (- (:y v1) (:y v2))))
+  (add v1 (scale v2 -1)))
 
 (defn magnitude
   [{x :x y :y}]
   (let [x2 (* x x)
         y2 (* y y)]
     (Math/sqrt (+ x2 y2))))
-
-(defn scale
-  [{x :x y :y} factor]
-  (make (* factor x) 
-        (* factor y)))
 
 (defn unit [v]
   (scale v (/ 1 (magnitude v))))
