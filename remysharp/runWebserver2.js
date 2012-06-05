f84b40310872f6146645ae082853ecf033da9b5a
Curtis Badke <curtis.badke@devfacto.com>
2012-06-05 13:44:38 +0200

    add basic router

--- remysharp/runWebserver2.js
+++ remysharp/runWebserver2.js
@@ -3,11 +3,20 @@ var connect = require('connect');
 var port = process.env.PORT || 8000;
 console.log('Starting server on port ' + port);
 
+var routes = function (app) {
+	app.get('/bob', function(req, res, next) {
+		console.log('hello bob');
+		res.writeHead(200, {'content-type': 'text/html'});
+		res.end('hello bob');
+	})
+};
+
 var server = connect()
 	.use(connect.logger())
 	.use(connect.favicon())
 	.use(connect.directory(__dirname + '/public'))
 	.use(connect.static(__dirname + '/public'))
+	.use(connect.router(routes))
 	.listen(port);
 
 
